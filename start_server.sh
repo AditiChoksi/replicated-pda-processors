56dd8c0962c6cb765809ea4f6d130d7b5570f025
Aditi Choksi <mail.aditichoksi@gmail.com>
2020-05-06 21:11:06 -0400

    updated startup script

--- start_server.sh
+++ start_server.sh
@@ -1,2 +1,2 @@
 echo "-----------------------Starting server-----------------" 
-go run objects_definitions.go helpers.go server.go
+go run objects_definitions.go helpers.go server.go replica.go main.go
